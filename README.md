6ebd4d47141f7df818b25dc33b01c353894074e7
Rajeev Singh <rsinghh68@gmail.com>
2023-09-18 23:52:33 +0530

    Revert "Revert "readme changes""
    This reverts commit 3b186e3f1e049c1bc0e75b9f612e9d964ac0b057.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # marketplace-project
 A backend project in go
+
+# Getting Started
+
+Following environment variables are required for Go-API-Boot to start up.
+
+```sh
+MONGO_URI=mongodb://localhost:27017
+```
--- server/odm/client.go
+++ server/odm/client.go
@@ -2,40 +2,29 @@ package odm
 
 import (
 	"context"
-	"crypto/tls"
+	"log"
 	"os"
-	"time"
 
-	"github.com/SaiNageswarS/go-api-boot/logger"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.uber.org/zap"
 )
 
 var connection *mongo.Client = nil
 
 func newMongoConn() *mongo.Client {
 	mongoUri := os.Getenv("MONGO_URI")
+	if mongoUri == "" {
+		log.Fatal("mongouri is empty")
+	}
 
 	mongoOpts := options.Client().ApplyURI(mongoUri)
-	mongoOpts.TLSConfig.MinVersion = tls.VersionTLS12
+	//mongoOpts.TLSConfig.MinVersion = tls.VersionTLS12
 	// make sure to install ca-certs in docker image.
 	// mongoOpts.TLSConfig.InsecureSkipVerify = true
 
-	client, err := mongo.NewClient(mongoOpts)
+	client, err := mongo.Connect(context.TODO(), mongoOpts)
 	if err != nil {
-		logger.Fatal("Failed to connect to mongo", zap.Error(err))
-	}
-
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		logger.Fatal("Failed to connect to mongo", zap.Error(err))
-	}
-
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		logger.Fatal("Failed to connect to mongo", zap.Error(err))
+		panic(err)
 	}
 
 	return client
