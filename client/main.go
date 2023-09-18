d35edba37c67d6c47191c8e46d347c075f67b0c8
Rajeev Singh <rsinghh68@gmail.com>
2023-09-18 23:43:32 +0530

    added client side code for searching of shops

--- client/main.go
+++ client/main.go
@@ -23,15 +23,38 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	AddShop(ctx, c)
+	// shopData := &pb.NewShopData{Name: "rdjshop", Users: []string{"65082c66e9a0c3dd7b76cb9e"}, Location: []float64{4.2, 5.5}, OperationTiming: []string{"12:00", "1:00"}}
+	// AddShop(ctx, c, shopData)
+	// FindShopById(ctx, c, "65082c66e9a0c3dd7b76cb9e")
+	FindShopNearMe(ctx, c, 4.2, 5.10005)
 
 }
 
-func AddShop(ctx context.Context, c pb.MarketplaceClient) {
-	shopData := &pb.NewShopData{Name: "rdjshop", Users: []string{"alice", "bob"}, Location: []float64{4.2, 5.5}, OperationTiming: []string{"12:00", "1:00"}}
+func AddShop(ctx context.Context, c pb.MarketplaceClient, shopData *pb.NewShopData) {
 	r, err := c.AddShop(ctx, shopData)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Data inserted", r)
+}
+
+func FindShopById(ctx context.Context, c pb.MarketplaceClient, idString string) {
+	id := &pb.Id{Id: idString}
+	r, err := c.FindShopById(ctx, id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Data Found", r.GetId())
+}
+
+func FindShopNearMe(ctx context.Context, c pb.MarketplaceClient, lattitude float64, longitude float64) {
+	loc := &pb.Location{Lattitude: lattitude, Longitude: longitude}
+	r, err := c.FindShopNearMe(ctx, loc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Shop near you are: ")
+	for _, x := range r.ShopData {
+		fmt.Printf("Shop name: %v  operation timing: %v\n", x.Name, x.OperationTiming)
+	}
 }
